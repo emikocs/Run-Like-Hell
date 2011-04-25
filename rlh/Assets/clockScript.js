8c58c8205ee373fe782f237fb430d27a59fb998a
Miko Charbonneau <evaliation@gmail.com>
2011-04-25 13:21:18 -0400

    Signed-off-by: Miko Charbonneau <evaliation@gmail.com>

---
+++ rlh/Assets/clockScript.js
@@ -0,0 +1,64 @@
+var isPaused : boolean = false;
+var startTime : float; //(in seconds)
+var timeRemaining : float; //(in seconds)
+function Start()
+{
+}
+
+function Update() {
+  if (!isPaused)
+  {
+    //startTime = 5.0;
+    // make sure the timer is not paused
+    DoCountdown();
+   }
+}
+
+
+
+function DoCountdown() {
+    timeRemaining = startTime - Time.time;
+    if (timeRemaining < 0.0)
+    {
+        timeRemaining = 0.0;
+        isPaused = true;
+        TimeIsUp();
+    }
+
+}
+
+function OnGUI(){
+
+    if (Camera.main.enabled == true)
+    {
+        ShowTime();
+    }
+
+}
+
+function PauseClock()
+{
+   isPaused = true;
+}
+
+function UnpauseClock()
+{
+   isPaused = false;
+}
+
+function ShowTime()
+{
+    var minutes : int;
+    var seconds : int;
+    var timeStr : String;
+    minutes = timeRemaining/60;
+    seconds = timeRemaining % 60;
+    timeStr = minutes.ToString() + ":";
+    timeStr += seconds.ToString("D2");
+    guiText.text = timeStr;
+}
+
+function TimeIsUp()
+{
+    Debug.Log("Time is up!");
+}
